3971af08199389d8ba1aa96d4ec9709e8ed69d1f
Yilber Toledo <26316784+yilbertoledo@users.noreply.github.com>
2020-05-25 20:44:37 -0400

    Tutorial section 5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,19 +17,20 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
   heroes: Hero[];
-  
+
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void{
+  //Event binding: Not in use
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
